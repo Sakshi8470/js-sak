fb8e1fed7edb274175ca0dfb4048015713d36487
Sakshi Mishra <132848417+Sakshi8470@users.noreply.github.com>
2025-01-25 06:05:48 +0000

    hey

---
+++ 01_basics/test.js
@@ -0,0 +1,25 @@
+const companyId = 46662
+let companyEmail = "km.mishra@naukri.com"
+var companyName = "Infoedge India Limited"
+
+companySector = 132
+
+//comapnyID = 12345
+
+//const companyId = 12345
+
+//let companyEmail = "sm@google.com"
+
+
+
+
+//companyId = 12345
+
+companyName = "Google"
+
+companyEmail = "sm.mishra@goggle.com"
+
+companySector = 135
+
+
+console.log([companyId , companyEmail,companyName, companySector]);
